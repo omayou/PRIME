af0519c9273ff96883f7dd10d9ab60717d3af80f
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-05 14:18:10 +0100

    Update Time_series.R

--- Time_series.R
+++ Time_series.R
@@ -4,4 +4,4 @@ time_index <- seq(from = as.POSIXct("2020-01-01 01:00:00"),
                   to = as.POSIXct("2020-01-03 23:00:00"), by = "min")
 random_values <- sample.int(10, length(time_index), replace = TRUE)
 df <- data.frame(date = time_index, Val = random_values)
-df_aggregated <- aggregate(df[names(df) != 'date'], list(hour = cut(df$date,'hour')), mean, na.rm=F)
+df_aggregated <- aggregate(df[names(df) != 'date'], list(hour = cut(df$date,'hour')), mean, na.rm = F)
