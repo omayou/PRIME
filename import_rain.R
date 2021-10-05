740e73df1ea6e654c007f0875f7d676857d285a9
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-05 17:13:39 +0100

    Update import_rain.R

--- import_rain.R
+++ import_rain.R
@@ -1,12 +1,12 @@
 import_rain <- function(rainfilename, startdate, enddate, timestep) {
   
-  rain <- read.csv(file = filename, header = FALSE) # Import rainfall data [date, rain(mm)]
-  rain <- rain[-(1:6),]                             # Remove the first 6 rows (data specification)
-  colnames(rain) <- c("date", "rain_mm")            # Adjust column names
+  rain <- read.csv(file = rainfilename, header = FALSE) # Import rainfall data [date, rain(mm)]
+  rain <- rain[-(1:6),]                                 # Remove the first 6 rows (data specification)
+  colnames(rain) <- c("date", "rain_mm")                # Adjust column names
   rain$date <- as.POSIXct(rain$date,format = "%d/%m/%Y %H:%M",tz = Sys.timezone())   # Change the Time type so that R can read it
   rain <- rain[rain$date >= startdate & 
-                 rain$date < enddate,]              # Select the period of interest
-  rain$rain_mm <- as.numeric(rain$rain_mm)          # Change the rain data type (from character) to number
+                 rain$date < enddate,]                  # Select the period of interest
+  rain$rain_mm <- as.numeric(rain$rain_mm)              # Change the rain data type (from character) to number
 
   if(timestep == 'hourly')
   {
