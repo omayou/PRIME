9d9e5b5ce7b3af07a0542c0a5efc444f9d7249c9
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-05 17:05:19 +0100

    Update import_rain.R

--- import_rain.R
+++ import_rain.R
@@ -10,11 +10,11 @@ import_rain <- function(rainfilename, startdate, enddate, timestep) {
 
   if(timestep == 'hourly')
   {
-    rain <- aggregate(rain[names(rain) != 'date'], list(hour = cut(rain$date, 'hour')), sum, na.rm = F)
+    rain <- aggregate(rain[names(rain) != 'date'], list(hourly = cut(rain$date, 'hour')), sum, na.rm = F)
   }
   else if (timestep == 'daily')
   {
-    rain <- aggregate(rain[names(rain) != 'date'], list(hour = cut(rain$date, 'day')), sum, na.rm = F)
+    rain <- aggregate(rain[names(rain) != 'date'], list(daily = cut(rain$date, 'day')), sum, na.rm = F)
   }
   else if (timestep == 'default')
   {
