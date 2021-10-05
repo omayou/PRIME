c850abb7f98a2ba696274ad871e943a341e1bb80
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-05 17:06:38 +0100

    Adding import_flow function

---
+++ import_flow.R
@@ -0,0 +1,40 @@
+import_flow <- function(flowfilename, startdate, enddate, timestep, flowunit) {
+  
+  flow <- read.csv(file = flowfilename, header = FALSE) # Import flow data [date, flow(liter per second)]
+  flow <- flow[-(1:6),]                                 # Remove first 6 rows (data specification)
+  flow <- flow[1:2]                                     # only select the first 2 columns, in case there are additional columns
+  colnames(flow) <- c("date", "flow_lps")               # Adjust column names
+  flow$date <- as.POSIXct(flow$date,format="%d/%m/%Y %H:%M",tz=Sys.timezone())   # Change the Time type so that R can read it
+  flow <- flow[flow$date >= startdate & 
+                 flow$date < enddate,]                  # Select the period of interest
+
+  flow$flow_lps <- as.numeric(flow$flow_lps)            # Change the flow data type (from character) to number
+  
+  if(timestep == 'hourly')
+  {
+    flow <- aggregate(flow[names(flow) != 'date'], list(hourly = cut(flow$date, 'hour')), mean, na.rm = F)
+  }
+  else if (timestep == 'daily')
+  {
+    flow <- aggregate(flow[names(flow) != 'date'], list(daily = cut(flow$date, 'day')), mean, na.rm = F)
+  }
+  else if (timestep == 'default')
+  {
+    
+  }
+  
+  
+    if (flowunit == 'cms')
+  {
+    flow$flow_lps <- flow$flow_lps/1000 
+    # update column name
+    colnames(flow) <- c("date", "flow_cms")
+  }
+  else if (flowunit == 'lps')
+  {
+    
+  }
+  
+  flow
+}
+
