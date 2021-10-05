59d19780283ca6f4f065b3a43fb26e8ae89c0caf
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-05 16:35:22 +0100

    Update import_rain.R

--- import_rain.R
+++ import_rain.R
@@ -3,7 +3,7 @@ import_rain <- function(filename, startdate, enddate, timestep) {
   rain <- read.csv(file = filename, header = FALSE) # Import rainfall data [date, rain(mm)]
   rain <- rain[-(1:6),]                             # Remove the first 6 rows (data specification)
   colnames(rain) <- c("date", "rain_mm")            # Adjust column names
-  rain$date <- as.POSIXct(rain$date,format="%d/%m/%Y %H:%M",tz=Sys.timezone())   # Change the Time type so that R can read it
+  rain$date <- as.POSIXct(rain$date,format = "%d/%m/%Y %H:%M",tz = Sys.timezone())   # Change the Time type so that R can read it
   rain <- rain[rain$date >= startdate & 
                  rain$date < enddate,]              # Select only the period of interest
   rain$rain_mm <- as.numeric(rain$rain_mm)          # Change the rain data type (from character) to number
