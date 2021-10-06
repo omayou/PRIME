1e96c8d9623d179e57a56c11c42bccb2e28b210b
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-10-06 09:06:55 +0100

    Create nearby_data.R

---
+++ nearby_data.R
@@ -0,0 +1,45 @@
+nearby_data <- function (station, satrtdate, enddate, whatdata, timestep) {
+  
+
+
+  
+  if (timestep == 'hourly') {
+    if (station == 'Mullingar') {
+      nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/hly875.csv",
+                      select = c("date", whatdata))
+    } else if (station == 'MtDillon') {
+      nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/hly1975.csv", 
+                      select = c("date", whatdata))
+   } else if (station == 'Ballyhaise') {
+     nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/hly675.csv", 
+                     select = c("date", whatdata))
+     }
+  }
+    
+    
+    else if (timestep == 'daily') {
+      if (station == 'Mullingar') {
+        nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/dly875.csv", 
+                        select = c("date", whatdata))
+      } else if (station == 'MtDillon') {
+        nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/dly1975.csv", 
+                        select = c("date", whatdata))
+      } else if (station == 'Ballyhaise') {
+        nearby <- fread("https://cli.fusio.net/cli/climate_data/webdata/dly675.csv", 
+                        select = c("date", whatdata))  
+      }
+    }
+ 
+ 
+  if (nchar(nearby[2,1]) >= 17) {
+    nearby$date <- as.POSIXct(nearby$date,format = "%d-%b-%Y  %H:%M",tz = Sys.timezone())
+  } else if (nchar(nearby[2,1]) <= 12) {
+    nearby$date <- as.POSIXct(nearby$date,format = "%d-%b-%Y",tz = Sys.timezone())
+  }
+  nearby <- nearby[nearby$date >= startdate & 
+               nearby$date < enddate,]    
+    
+    nearby
+   
+}
+
