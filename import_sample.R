6b56cd5b5f8b49b783b53865fb98f2915a6c99eb
Behzad Mozafari <66364577+omayou@users.noreply.github.com>
2021-12-08 18:29:19 +0000

    Create import_sample.R

---
+++ import_sample.R
@@ -0,0 +1,40 @@
+import_sample <- function(samplefilename, flow) {
+  
+  sampleevent <- read.csv(file = rainfilename, header = FALSE) # Import sample event data [date, number of samples]
+  colnames(sampleevent) <- c("date", "number")                # Adjust column names
+  sampleevent$date <- as.POSIXct(sampleevent$date,format = "%d/%m/%Y %H:%M:%S",tz = Sys.timezone())   # Change the Time type so that R can read it
+
+  sampleevent$number <- as.numeric(sampleevent$number)              # to make sure sample numbers are numeric
+  
+  
+  
+  # closest time to each sampling event should be first initialized:
+  ClosestTimeToSample <- as.POSIXct("1990-01-01 00:00:00")
+  
+  # find above time for each sampling event
+  for (i in 1:nrow(sampleevent)) {
+    ClosestTimeToSample[i] = flow$date[which.min(abs(difftime(flow$date, sampleevent$date[i], units = "secs")))]
+  }
+  # find the below interval as well as the time distance to above limit to calculate the abstract flow for plotting
+  for (i in 1:nrow(sampleevent)) {
+    
+    if (ClosestTimeToSample[i] >= sampleevent$date[i]) {
+      above.t = ClosestTimeToSample[i]
+      below.t = time.series[which(time.series == ClosestTimeToSample[i])-1]
+    } else {
+      below.t = ClosestTimeToSample[i]
+      above.t = time.series[which(time.series == ClosestTimeToSample[i])+1]
+    }
+    ToAbovePortion = as.numeric(difftime(above.t, sampleevent$date[i], units = "secs")) / as.numeric(difftime(above.t, below.t, units = "secs"))
+    
+    sampleevent_above_flow <- flow[flow$date %in% above.t, 2]
+    sampleevent_below_flow <- flow[flow$date %in% below.t, 2]
+    sampleevent_flow <- sampleevent_above_flow - (sampleevent_above_flow - sampleevent_below_flow) * ToAbovePortion
+    
+    
+    sampleevent[i, 3] <- sampleevent_flow
+    
+  }
+  
+  sampleevent
+}
